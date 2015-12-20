24e8c828ed3de12596701850c7810bc2bffc266d
overrflow <reptiliansquad@protonmail.ch>
2015-12-20 10:08:15 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,71 @@
+'''this is the unoffical port of the highly play game impossible quiz by 0verrflow @0verrflow on twitter'''
+ 
+ 
+#disclaimer: this is only the first 20 question whoever feels like adding more can but i'd like some credit in the final product
+ 
+ 
+print('rules:\n1)pick the correct answer\n2)have fun!!')
+idk = input('are you ready?\n(yes or no)\n')
+if idk=="yes":print('Ok first question starts now')
+elif idk=="no":print('Well too bad the game starts anyway')
+else:print('That was not the answer I was looking for and the game is starting anyway')
+ 
+print("FIRST QUESTION(Please enter in the answer correctly with capitals or it won't count)")
+print('How Many Holes Are In A Polo?\n')
+question_one = input('One\nTwo\nThree\nFour\n')
+if question_one=="Four":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nSECOND QUESTION')
+print('Can A Match Box?\n')
+question_two = input('Yes\nNo\nNo, But A Tin Can\nYes, One Beat Mike Tyson\n')
+if question_two=="No, But A Tin Can":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nTHIRD QUESTION')
+print('.SDRAWKCAB NOITSEUQ SIHT REWSNA\n')
+question_three = input("K.O.\nWhat?\nI Don't Understand\nTennis Elbow\n")
+if question_three=="K.O.":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nFOURTH QUESTION') #actually sixth question but 4 and 5 were visual questions that I wasn't able to put in here
+print('√Onion\n')
+question_four = input('28\nCarrot\nShallots\nπ\n')
+if question_four=="Shallots":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nFIFTH QUESTION')
+print('The Answer Is Really Big\n')
+question_five = input('ANSWER\nReally Big\n∞\nAn Elephant\n')
+if question_five=="An Elephant":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nSIXTH QUESTION')
+print('What Was The Answer To Question 2?\n')
+question_six = input('1\n2\n3\n4\n')
+if question_six=="2":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nSEVENTH QUESTION')
+print('Choose Food\n')
+question_seven =input('Glove\nMouth\nEyeball\nChair\nPen\n')
+if question_seven=="Mouth":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nEIGHTH QUESTION')
+print('What Follows December 2nd?\n')
+question_eight = input('December 3rd\nn\nA Question Mark\n142 Dwarves\n')
+if question_eight=="n":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nNINTH QUESTION')
+print('What Sound Does A Bell Make?\n')
+question_nine = input("Whoop\nF'Taang\nFroon\nBlip-Blop-Bloop-Banga-O-Langa-Woof\n") #call me lazy but i didn't want to put the whole last option it was way too fucking long
+if question_nine=="F'Taang":print('Correct!')
+else:print('Sorry Incorrect')
+ 
+print('\nTENTH QUESTION')
+print('What Can You Put In A Bucket To Make It Lighter?\n')
+question_ten = input('Gypsies\nTorch\nA Hole\nCanned Laughter\n')
+if question_ten=="Torch":print('Correct!')
+else:print('Sorry Incorrect')
